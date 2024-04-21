aa0e67d7ad918df119dfef56937dcadccb6b3d40
Aliona Kragel <aliona.kragel@mail.ru>
2024-04-22 01:41:48 +0300

    init: delete default files and install some libraries

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { history, store } from './store/configure-store';
+import { HistoryRouter } from "redux-first-history/rr6";
+import { routes } from './router';
+
+import "./styles/styles.module.scss"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <HistoryRouter history={history}>{routes}</HistoryRouter>
+    </Provider>
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
