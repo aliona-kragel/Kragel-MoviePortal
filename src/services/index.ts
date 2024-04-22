32bd405c70100c3899469a350c08411f6b547e03
Aliona Kragel <aliona.kragel@mail.ru>
2024-04-22 03:04:04 +0300

    feat: add request for movie list and loader

---
+++ src/services/index.ts
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { API_KEY, BASE_API_URL } from '../constants';
+import { GetMoviesParams, ResponceListType } from '../types/types';
+
+export const moviesApi = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_API_URL }),
+  endpoints: (builder) => ({
+    getMovies: builder.query<ResponceListType, GetMoviesParams>({
+      query: ({ page = 1, limit = 10 }) =>
+        `/?apikey=${API_KEY}&s=series&page=${page}&limit=${limit}`,
+    }),
+  }),
+});
+
+export const { useGetMoviesQuery } = moviesApi;
